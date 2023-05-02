383f4c41367e9f46c23f5f0c639761fdb7447b7d
JulianaSobaJava <julianasobajava23@gmail.com>
2023-05-02 15:39:55 +0100

    :fire: add mock file into the project

---
+++ src/utils/mocks.ts
@@ -0,0 +1,5 @@
+export const data = [
+  "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+];
