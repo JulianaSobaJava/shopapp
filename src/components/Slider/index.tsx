a4d3ab299a1eac5c8e7abeedba53d50b70497336
JulianaSobaJava <julianasobajava23@gmail.com>
2023-05-02 15:44:01 +0100

    :tada: add slide component and styles

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,32 @@
+import {data, Icon, useState} from "../../utils/exports";
+import "./Slider.scss"
+
+export default function Slider() {
+
+  const [currentSlider, setCurrentSlider] = useState(0);
+
+  const prevSlide=()=>{
+    setCurrentSlider(currentSlider=== 0 ? 2 : (prev)=> prev - 1);
+  
+  }
+  const nextSlide=()=>{
+    setCurrentSlider(currentSlider=== 2 ? 0 : (prev)=> prev + 1)
+  }
+  return (
+    <div className="slider">
+      <div className="container" style={{transform:`translateX(-${currentSlider*100}vw)`}}>
+        <img src={data[0]} alt="" className="image_banner" />
+        <img src={data[1]} alt="" className="image_banner" />
+        <img src={data[2]} alt="" className="image_banner" />
+      </div>
+      <div className="icons">
+        <div className="icon" onClick={prevSlide}>
+          <Icon.WestOutlined/>
+        </div>
+        <div className="icon" onClick={nextSlide}>
+          <Icon.EastOutlined/>
+        </div>
+      </div>
+    </div>
+  )
+}
